6f76c82e9bd099aa44bd88fb4a2fdbe1d0828d27
CRUD0626 <72868495+crud0626@users.noreply.github.com>
2024-04-26 19:48:24 -0700

    Time: 98 ms (65.65%), Space: 54.1 MB (83.51%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -1,13 +1,13 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 const arrayPairSum = function(nums) {
-    let result = 0;
+    let sum = 0;
     nums.sort((a, b) => a - b);
 
-    for(let i = 0; i < nums.length; i += 2) {
-        result += nums[i];
+    for(let i = 0; i < nums.length; i+=2) {
+        sum += nums[i];
     }
-    return result;
+    
+    return sum;
 };
+
+// 오름차순으로 정렬
+// nums.length / 2만큼을 sum하면 될 듯?
